ed9667f3bbd5469b5945e755c4794b318a2971f8
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2021-03-25 17:00:24 +0000

    linear

--- Linear_Search.js
+++ Linear_Search.js
@@ -1,12 +1,14 @@
 
 function LinearSearch(list, target) {
-   
+    for (let i = 0; i < list.length; i++) {
         if (list[i] === target) {
-            console.log(list[i]);
+            console.log(i);
+            break;
         }
         else console.log("Target not found");
-   
+        break;
+   }
 }
 
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-LinearSearch(list, 1);
+LinearSearch(list, 11);
