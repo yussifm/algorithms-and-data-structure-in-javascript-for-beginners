0ef3f4bbd2db92a44e2aa0c51e3ab90ac73d5ca7
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2021-03-25 16:09:41 +0000

    linearSearch

---
+++ Linear_Search.js
@@ -0,0 +1,12 @@
+
+function LinearSearch(list, target) {
+   
+        if (list[i] === target) {
+            console.log(list[i]);
+        }
+        else console.log("Target not found");
+   
+}
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+LinearSearch(list, 1);
---
+++ README.md
@@ -0,0 +1,5 @@
+#Algorithms and Data structures in javascript
+
+This repo is full Algorithms and Data structures for JavaScript;
+
+#Author: Yussif Mohammed(CODED)
